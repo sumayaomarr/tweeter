aa72e091ea1828b9caa61e56d170a1a93bacbef2
sumaya <sumayaomarr@gmail.com>
2021-12-16 02:52:31 -0500

    implented character count and added data to html file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,90 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+  console.log( "ready!" );
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  
+  const renderTweets = function (data) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    container = $('#feed')
+    data.forEach(function (tweet) {
+      let $tweet = createTweetElement(tweet)
+      container.prepend($tweet)
+    })
+  }
+  
+  
+  
+  
+  
+  const createTweetElement = function (tweet) { 
+      let name = tweet.user.name
+      let avatars = tweet.user.avatars
+      let handle = tweet.user.handle
+      let content = tweet.content.text
+      let timestamp = timeago.format(tweet.created_at);
+  let $tweet = `
+  <article class="tweet">
+  <header> 
+      <img src=${avatars}></img>
+    <p> ${name} </p>
+    <p> ${handle} </p>
+  
+  </header>
+  
+  <p class="tweet-text">${content}</p>
+  
+  
+  <footer> 
+    <p>${timestamp}</p>
+  
+    <div class='icons'>
+    <i class="fas fa-flag"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="fas fa-heart"></i>
+  
+  </div>
+  
+  
+  </footer>
+  </article>
+   `
+   return $tweet;
+  }
+  
+  renderTweets(data)
+  
+  
+});
+
+
